a179b9df30c50c9e6aa1726218156b490d52a2e5
hubertDgomes <gomeshubert8@gmai.com>
2025-07-08 21:45:21 +0600

    feat(ArivalPart): implement Products component for product display with interactive features

--- src/assets/components/layouts/ArivalPart.jsx
+++ src/assets/components/layouts/ArivalPart.jsx
@@ -1,26 +1,45 @@
-import React from 'react'
-import Container from '../Container'
-import Flex from '../Flex'
-import Images from '../Images'
-import Pro1 from '../../pro1.png'
-import Pro2 from '../../pro2.png'
-import Pro3 from '../../pro3.png'
-import Pro4 from '../../pro4.png'
-import Pro5 from '../../pro5.png'
-import Pro6 from '../../pro6.png'
-import Pro7 from '../../pro7.png'
-import Pro8 from '../../pro8.png'
-import { FaCircleLeft } from "react-icons/fa6";
-import add2 from '../../Ads_2.png'
+import React from "react";
+import Container from "../Container";
+import Products from "./Products";
+import Flex from "../Flex";
+import pro1 from '/src/assets/pro1.png'
+import pro2 from '/src/assets/pro2.png'
+import pro3 from '/src/assets/pro3.png'
+import pro4 from '/src/assets/pro4.png'
+
 const ArivalPart = () => {
   return (
     <div>
       <Container className={"py-[20px]"}>
-        
-        
+        <Flex className={"gap-x-[20px]"}>
+          <Products
+            badgeValue={"10%"}
+            productName={"Basic Crew Neck Tee"}
+            productPrice={"44.00"}
+            src={pro1}
+          />
+          <Products
+            badgeValue={"New"}
+            productName={"Basic Crew Neck Tee"}
+            productPrice={"44.00"}
+            src={pro2}
+          />
+          <Products
+            badgeValue={"New"}
+            productName={"Basic Crew Neck Tee"}
+            productPrice={"44.00"}
+            src={pro3}
+          />
+          <Products
+            badgeValue={"New"}
+            productName={"Basic Crew Neck Tee"}
+            productPrice={"44.00"}
+            src={pro4}
+          />
+        </Flex>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default ArivalPart
+export default ArivalPart;
---
+++ src/assets/components/layouts/Products.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Images from '../Images'
+import pro1 from '/src/assets/pro1.png'
+import { FaHeart } from "react-icons/fa";
+import { FaSyncAlt } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import Flex from '../Flex';
+const Products = ({badgeValue,productName,productPrice,src}) => {
+  return (
+    <div className="relative w-[370px] group">
+        <Images src={src}/>
+        <div className="py-[10px] px-[30px] bg-black text-white w-[100px] text-center absolute top-4 left-4">{badgeValue}</div>
+        <div className="w-full py-[30px] px-[25px] bg-white absolute bottom-[74px] opacity-0 group-hover:opacity-100 translate-y-4 group-hover:translate-y-0 transition-all duration-500 ease-in-out cursor-pointer">
+            <Flex className={"gap-x-[10px] justify-end"}>
+                <p className={`font-menu text-[16px] text-kala3 hover:font-bold hover:text-black`}>Add to Wish List </p><FaHeart />
+            </Flex>
+            <Flex className={"gap-x-[10px] justify-end my-[10px]"}>
+                <p className={`font-menu text-[16px] text-kala3 hover:font-bold hover:text-black`}>Compare </p><FaSyncAlt />
+            </Flex>
+            <Flex className={"gap-x-[10px] justify-end"}>
+                <p className={`font-menu text-[16px] text-kala3 hover:font-bold hover:text-black`}>Add to Cart</p><FaShoppingCart />
+            </Flex> 
+        </div>
+        <div className="py-[25px] bg-white flex justify-between items-center">
+            <p className='font-bold text-[20px] font-menu'>{productName}</p>
+            <p className='font-menu text-[16px] text-kala3'>{"$"+productPrice}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Products
