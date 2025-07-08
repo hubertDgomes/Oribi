a3a96a1f6166ef9480750e1bf211ab87d067f1fc
hubertDgomes <gomeshubert8@gmai.com>
2025-07-08 22:04:43 +0600

    feat(ArivalPart): integrate react-slick for product carousel and adjust product component layout

--- src/assets/components/layouts/ArivalPart.jsx
+++ src/assets/components/layouts/ArivalPart.jsx
@@ -2,41 +2,68 @@ import React from "react";
 import Container from "../Container";
 import Products from "./Products";
 import Flex from "../Flex";
-import pro1 from '/src/assets/pro1.png'
-import pro2 from '/src/assets/pro2.png'
-import pro3 from '/src/assets/pro3.png'
-import pro4 from '/src/assets/pro4.png'
+import pro1 from "/src/assets/pro1.png";
+import pro2 from "/src/assets/pro2.png";
+import pro3 from "/src/assets/pro3.png";
+import pro4 from "/src/assets/pro4.png";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
 
 const ArivalPart = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  };
   return (
     <div>
       <Container className={"py-[20px]"}>
-        <Flex className={"gap-x-[20px]"}>
-          <Products
-            badgeValue={"10%"}
-            productName={"Basic Crew Neck Tee"}
-            productPrice={"44.00"}
-            src={pro1}
-          />
-          <Products
-            badgeValue={"New"}
-            productName={"Basic Crew Neck Tee"}
-            productPrice={"44.00"}
-            src={pro2}
-          />
-          <Products
-            badgeValue={"New"}
-            productName={"Basic Crew Neck Tee"}
-            productPrice={"44.00"}
-            src={pro3}
-          />
-          <Products
-            badgeValue={"New"}
-            productName={"Basic Crew Neck Tee"}
-            productPrice={"44.00"}
-            src={pro4}
-          />
-        </Flex>
+        <Slider {...settings}>
+          <div>
+            <Products
+              badgeValue={"10%"}
+              productName={"Basic Crew Neck Tee"}
+              productPrice={"44.00"}
+              src={pro1}
+            />
+          </div>
+          <div>
+            <Products
+              badgeValue={"New"}
+              productName={"Basic Crew Neck Tee"}
+              productPrice={"44.00"}
+              src={pro2}
+            />
+          </div>
+          <div>
+            <Products
+              badgeValue={"New"}
+              productName={"Basic Crew Neck Tee"}
+              productPrice={"44.00"}
+              src={pro3}
+            />
+          </div>
+          <div>
+            <Products
+              badgeValue={"New"}
+              productName={"Basic Crew Neck Tee"}
+              productPrice={"44.00"}
+              src={pro4}
+            />
+          </div>
+          <div>
+            <Products
+              badgeValue={"New"}
+              productName={"Basic Crew Neck Tee"}
+              productPrice={"44.00"}
+              src={pro4}
+            />
+          </div>
+        </Slider>
       </Container>
     </div>
   );
