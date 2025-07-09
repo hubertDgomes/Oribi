3ada144032eeacd9dc9268ca737eec6d4bed59cb
hubertDgomes <gomeshubert8@gmai.com>
2025-07-09 11:42:30 +0600

    Add progress bar on the top in the Home section

--- src/assets/components/pages/Home.jsx
+++ src/assets/components/pages/Home.jsx
@@ -3,11 +3,20 @@ import Header from '../layouts/Header'
 import Home2 from '../layouts/Home2'
 import ArivalPart from '../layouts/ArivalPart'
 import Land from '../layouts/Land'
-
+import { motion, useScroll } from "motion/react"
 
 const  Home = () => {
+
+  const scrollY = useScroll().scrollYProgress
+   
+
   return (
     <>
+    <motion.div
+    style={{
+      scaleX:scrollY
+    }}
+    className="h-[10px] w-full fixed origin-left bg-kala3 top-0 left-0 z-50"></motion.div>
     <Land/>
     <Home2/>
     <ArivalPart/>
